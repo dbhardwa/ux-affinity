bdf6a4e2f9e09e744288efcb4b07df1bbabe73bc
Devansh Bhardwaj <devansh.bhardwaj@outlook.com>
2018-11-30 11:59:28 -0500

    Uploads excel sheet and parses it to a workable object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import "./scss/main.scss";
+import * as d3 from "d3";
+import XLSX from 'xlsx';
 
-var particles = ['proton', 'neutron', 'electron'];
+let fileInput = d3.select('body')
+                  .append('input')
+                  .attr('type', 'file')
+                  .attr('id', 'fileInput')
+                  .on('change', handleFileUpload, false);
 
-particles.forEach((particle) => {
-  console.log(particle + 's');
-});
+
+function handleFileUpload() {
+  let file = this.files[0],
+      reader = new FileReader();
+
+  // FileReader instance parses raw file.
+  reader.readAsBinaryString(file);
+
+  // Triggers when reader has read the file.
+  reader.onload = function(e) {
+    let data = e.target.result;
+    // XLSX parses raw cryptic data.
+    let workbook = XLSX.read(data, {type: 'binary'});
+
+    console.log(workbook);
+  };
+}
